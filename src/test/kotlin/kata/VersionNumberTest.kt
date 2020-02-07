4a1e34e7d6b705089d27f552ebb8fb5ac943f2ab
Jørgen Rognerud <jorgen.rognerud@knowit.no>
2020-02-07 14:01:32 +0100

    asasdasd beta

--- src/test/kotlin/kata/VersionNumberTest.kt
+++ src/test/kotlin/kata/VersionNumberTest.kt
@@ -23,10 +23,11 @@ class VersionNumberTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["1.2.3", "3.2.1", "1.2.3-alpha"]) // six numbers
+    @ValueSource(strings = ["1.2.3", "3.2.1", "1.2.3-alpha", "3.2.1-beta"]) // six numbers
     fun `Test multiple versions`(str: String) {
         val version = VersionNumber.from(str)
         val elements = str.split(".", "-")
+
         assertEquals(version.major, elements[0].toInt())
         assertEquals(version.minor, elements[1].toInt())
         assertEquals(version.patch, elements[2].toInt())
