f2a609d20c3ccba789703a1fb852c844a7b245fa
Torsph <thomas@torp.io>
2020-02-07 14:03:31 +0100

    Handle alpha and beta

--- src/main/kotlin/kata/VersionNumber.kt
+++ src/main/kotlin/kata/VersionNumber.kt
@@ -17,7 +17,7 @@ data class VersionNumber(
 
         fun from(str : String): VersionNumber {
             val versionNumberList = str.split(".", "-")
-            return VersionNumber(versionNumberList[0].toInt(), versionNumberList[1].toInt(), versionNumberList[2].toInt(), "alpha")
+            return VersionNumber(versionNumberList[0].toInt(), versionNumberList[1].toInt(), versionNumberList[2].toInt(), versionNumberList.elementAtOrNull(3))
         }
 
     }
