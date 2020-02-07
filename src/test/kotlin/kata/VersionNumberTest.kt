393680833c809faa5efbda59fbf11e090b25a847
Jørgen Rognerud <jorgen.rognerud@knowit.no>
2020-02-07 14:32:07 +0100

    asd

--- src/test/kotlin/kata/VersionNumberTest.kt
+++ src/test/kotlin/kata/VersionNumberTest.kt
@@ -46,7 +46,22 @@ class VersionNumberTest {
         assertEquals(version.minor, elements[1].toInt())
         assertEquals(version.patch, elements[2].toInt())
         if (elements.count() > 3) {
-            assertEquals(version.buildInfo, elements[3].toInt())
+            assertEquals(version.buildInfo, elements[3])
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1.2.3", "3.2.1", "1.2.3-alpha.1"]) // six numbers
+    fun `Test pre release and build info`(str: String) {
+        val version = VersionNumber.from(str)
+        val elements = str.split(".", "-", "+")
+        println(elements)
+        assertEquals(version.major, elements[0].toInt())
+        assertEquals(version.minor, elements[1].toInt())
+        assertEquals(version.patch, elements[2].toInt())
+        if (elements.count() > 4) {
+            assertEquals(version.preRelease, elements[3])
+            assertEquals(version.buildInfo, elements[4])
         }
     }
 
