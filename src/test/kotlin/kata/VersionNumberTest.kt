57c1d9fbb030baab7b272e03d21a93c83381e8dc
Kim A. Betti <kim.betti@byr.oslo.kommune.no>
2020-02-07 14:50:35 +0100

    Pretty name

--- src/test/kotlin/kata/VersionNumberTest.kt
+++ src/test/kotlin/kata/VersionNumberTest.kt
@@ -1,12 +1,14 @@
 package kata
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 import java.util.stream.Stream
 
+@DisplayName("Pent testnavn på klassen")
 class VersionNumberTest {
 
     @Test
@@ -105,7 +107,6 @@ class VersionNumberTest {
     data class AddTest(val a: Int, val b: Int, val expectedSum: Int) {
 
         fun runTest() {
-            println(toString())
             assertEquals(expectedSum, a + b)
         }
 
